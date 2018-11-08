0d11de25901e7d8a1eb54053ad202c530878ac88
Luiz Carlos Muniz <lcmuniz@gmail.com>
2018-11-07 21:08:39 -0300

    Create exemplo10.c

---
+++ exemplo10.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+
+// Receber um nome e imprimí-lo de trás pra frente.
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("e09: Recebe um nome e imprimí-lo de trás pra frente.\n");
+        printf("Uso: e09 <nome>\n");
+        return 1;
+    }
+
+    char *nome = argv[1];
+    int tam = strlen(nome);
+
+    for (int i = tam; i >= 0; i--)
+    {
+        printf("%c", nome[i]);
+    }
+
+    printf("\n");
+
+}
+
+
