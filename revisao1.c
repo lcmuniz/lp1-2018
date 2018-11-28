df5c4ec9bfd759b2c07c871a0e5a6eb843e1d10c
Luiz Carlos Muniz <lcmuniz@gmail.com>
2018-11-28 19:21:34 -0300

    Create revisao1.c

---
+++ revisao1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    int q = 5;
+    
+    for (int i = 0; i < q; i++)
+    {
+        printf("A[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    
+    printf("A invertido = ");
+    for (int i = q - 1; i >= 0; i--) 
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
